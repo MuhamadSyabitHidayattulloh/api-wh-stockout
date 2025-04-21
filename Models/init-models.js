8ddc2653a1b2c13738ae2d0bd7c96fd9299cc17f
Bravianto <bravianto.luhur@gmail.com>
2025-04-21 20:10:23 +0700

    add WH_T_FIFO

--- Models/init-models.js
+++ Models/init-models.js
@@ -1,12 +1,12 @@
 var DataTypes = require("sequelize").DataTypes;
-var _WH_T_TEMPORARY = require("./WH_T_TEMPORARY");
+var _WH_T_FIFO = require("./WH_T_FIFO");
 
 function initModels(sequelize) {
-  var WH_T_TEMPORARY = _WH_T_TEMPORARY(sequelize, DataTypes);
+  var WH_T_FIFO = _WH_T_FIFO(sequelize, DataTypes);
 
 
   return {
-    WH_T_TEMPORARY,
+    WH_T_FIFO,
   };
 }
 module.exports = initModels;
