e09b6569ba31ee82ddcda92318417a4aa4d9c624
Bravianto <bravianto.luhur@gmail.com>
2025-07-16 16:50:48 +0700

    delete unnecessary comment

--- Controller/warehouse.js
+++ Controller/warehouse.js
@@ -282,7 +282,6 @@ export const stockoutInstructionController = async (req, res) => {
 export const stoctkoutAndroidWHSystem = async (req, res) => {
   try {
     const data = req.body.data;
-    // console.log(data);
     const slip = req.body.slip;
 
     if (!data?.length) {
@@ -295,8 +294,6 @@ export const stoctkoutAndroidWHSystem = async (req, res) => {
       const formattedTime = moment(item.timeScan).format("HH:mm:ss");
 
       return {
-        // imgData: item.imgData,
-        // timeScan: item.timeScan,
         SLIP: slip || null,
         NPK: item.NPK,
         PARTNO: oneWayKanban.getTotalPartNumber(),
@@ -310,48 +307,6 @@ export const stoctkoutAndroidWHSystem = async (req, res) => {
         FLAGDX: 0,
       };
     });
-
-    // const existingData = await STOCKOUT_T_TRANSACTION_2.findAll({
-    //   where: {
-    //     [Op.or]: bulkData.map((item) => ({
-    //       [Op.and]: {
-    //         PARTNO: item.PARTNO,
-    //         SQ: item.SQ,
-    //         WH: item.WH,
-    //       },
-    //     })),
-    //   },
-    //   attributes: {
-    //     exclude: ["id"],
-    //   },
-    // });
-
-    // const existingSet = new Set(
-    //   existingData.map((item) => `${item.PARTNO}-${item.SQ}-${item.WH}`)
-    // );
-
-    // console.log("data yang sama nih: ", existingSet);
-
-    // const newData = bulkData.filter(
-    //   (item) => !existingSet.has(`${item.PARTNO}-${item.SQ}-${item.WH}`)
-    // );
-
-    // console.log("data yang boleh di stockout nih: ", newData);
-
-    // console.log(`Total data: ${bulkData.length}`);
-    // console.log(`Data yang sudah ada: ${existingData.length}`);
-    // console.log(`Data baru yang akan diinsert: ${newData.length}`);
-
-    // if (newData.length > 0) {
-    //   await STOCKOUT_T_TRANSACTION_2.bulkCreate(newData, { returning: false });
-    //   // Add job ke queue
-    //   await stockoutQueue.add({
-    //     data: newData,
-    //     NPK: data[0].NPK,
-    //     timeScan: data[0].timeScan,
-    //   });
-    // }
-
     // Bulk insert ke database
     await STOCKOUT_T_TRANSACTION_2.bulkCreate(bulkData, { returning: false });
 
